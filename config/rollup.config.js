b4c9e303c3bf289d545ba8c5d713e53955e1e38c
Leon de Rijke <leon@leonderijke.nl>
2017-02-27 14:32:28 +0100

    Add version to generated files

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,12 +1,15 @@
 import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace'
 
+const pkg = require('../package.json')
+
 export default config => {
   return {
     entry: 'src/index.js',
     format: config.format,
     moduleName: 'npoApiInterceptor',
     dest: config.dest,
+    banner: `/*! NPO API Interceptor - v${pkg.version} */`,
     external: [ 'jssha' ],
     globals: {
       jssha: 'jsSHA'
